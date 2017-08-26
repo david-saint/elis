27f833a50bc39d0cda97f71a6dfbd34c792ed656
kampkelly <kampkellykeys@gmail.com>
2017-08-26 12:21:45 +0100

    color

--- resources/views/partials/_top-nav.blade.php
+++ resources/views/partials/_top-nav.blade.php
@@ -1,7 +1,9 @@
 <div class="navbar-fixed">
-    <nav>
+    <nav class>
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+
         <div class="nav-wrapper">
-            <a href="" class="brand-logo center">Logo here</a>
+            <a href="" class="brand-logo center"><span style="font-family: Orbitron ">Elis </span><span style="font-size: 15px;">- Find business and products around you!</span></a>
             <a href="" class="left"><i class="material-icons">person</i></a>
             <a href="" class="dropdown-button right" data-activates="more_content" data-constrainWidth="false"><i class="material-icons">more_vert</i></a>
         </div>
